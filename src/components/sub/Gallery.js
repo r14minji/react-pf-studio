b0b3646123eaf49f08d62276b02414537f7a92c9
r14minji <rkdalswl0728@gmail.com>
2022-01-02 17:14:56 +0900

    gallery 기본 갤러리 기능

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,32 +1,29 @@
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
-//import Masonry from "react-masonry-component";
+import Masonry from "react-masonry-component";
 
 const path = process.env.PUBLIC_URL;
 const body = document.querySelector("body");
 
 function Gallery(){
+  let [items, setItems] = useState([]);
+  let [index, setIndex] = useState(0);
+  let [isPop, setIsPop] = useState(false);
+
+
   const baseURL = "https://www.flickr.com/services/rest/?";
   const method1 = "flickr.interestingness.getList";
   const method2 = "flickr.photos.search";
   const key= "e7ed3b39fe112d7e93d03c19325305e0";
-  const count = 10;
-  const url = `${baseURL}method=${method1}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1`;  
-
-  let [items, setItems] = useState([]);
-  let [loading, setLoading] = useState(true);
-  let [enableClick, setEnableClick] = useState(true); 
-  let [interest, setInterest] = useState(true);
-  let [isPop, setIsPop] = useState(false);
-  let [index, setIndex] = useState(0);
-  let list = useRef(null);
-  let input = useRef(null);
+  const count =30;
+  const url = `${baseURL}method=${method1}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1`;
 
   useEffect(()=>{
     axios
     .get(url)
-    .then(data =>{
-      setItems(data.data.photos.photo);
+    .then(json =>{
+      //console.log(json.data.photos.photo);
+      setItems(json.data.photos.photo);
     })
   },[])
 
@@ -35,23 +32,19 @@ function Gallery(){
       <div className="inner">
         <div className="title_wrap">
           <p>Should I take a photo?</p>
-          <h1>&#8546;. Gallery</h1>
+          <h1>&#8546;. GALLERY</h1>
         </div>
 
         <article className="searchBox">
-          <input type="text"  id="search" placeholder="검색어 입력" ref={input} />
+          <input type="text"  id="search" placeholder="검색어 입력" />
           <button className="btnSearch" >Search</button>
         </article>
 
-        {(loading) ? <img src={path+"/img/loading.gif"} alt="로딩중입니다." className="loading" /> : ""}
-
-        <div className="list" ref={list}>
+        <div className="list">
           <ul className="wrap">
             {
-              items.map((item, index) =>{
-                const imgSrc =  `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`;
-                const imgSrcBic =  `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`;
-
+              items.map((item, index)=>{
+                const imgSrc =  `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`;                
                 return(
                   <li key={index} className="item">
                     <figure>
@@ -61,18 +54,36 @@ function Gallery(){
                       <figcaption>
                         <p>{item.title}</p>
                         <span>{item.owner}</span>
-                        <a href={imgSrcBic}>VIEW MORE</a>
+                        <div className="btnMore" onClick={()=>{
+                          setIsPop(true);
+                          setIndex(index);
+                        }}>VIEW MORE</div>
                       </figcaption>
                     </figure>                
                   </li>
                 )
               })
             }
-            </ul>
-        </div>
+          </ul>
       </div>
+      </div>
+
+      { isPop ? <Pop /> : "" }
     </section>
   )
+
+  function Pop(){
+    const imgSrc= `https://live.staticflickr.com/${items[index].server}/${items[index].id}_${items[index].secret}_b.jpg`
+    return(
+      <aside className="pop">
+        <img src={imgSrc} />
+        <p>{}</p>
+        <span onClick={()=>{
+          setIsPop(false);
+        }}>Close</span>
+      </aside>
+    )
+  }
 }
 
 export default Gallery;
