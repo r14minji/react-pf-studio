12016044a3bb78e49c186d331d3d051f09d41255
r14minji <rkdalswl0728@gmail.com>
2022-01-05 23:44:44 +0900

    contact title

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -1,10 +1,24 @@
 function Contact(){
   return(
-    <section className="content contact">
+    <>
+    {/* title */}
+    <section className="contact">
       <div className="inner">
-      <h1>&#8548;. Contact</h1>
+        <div className="title">
+          <h1>&#8548;.Contact</h1>
+          <p>contact us to get more information about studioalice</p>
+        </div>
+          <div className="memo">
+          <span>Sno.30</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat optio, excepturi a dolorem.</p>
+        </div>
+        <div className="numbering">
+          <p>Lorem ipsum<br />amet consectetur <br />elit. Ea, illo!</p>
+          <strong>#0001</strong>
+        </div>
       </div>
     </section>
+    </>
   )
 }
 
