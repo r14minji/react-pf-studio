28685f24608d84a7bbca92ed31cd53d8eb1c6730
r14minji <rkdalswl0728@gmail.com>
2022-01-15 21:47:46 +0900

    반응형 visual 완료

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,10 +1,43 @@
+import { faRssSquare, faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import {faFacebookSquare, faTwitterSquare, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const path = process.env.PUBLIC_URL;
 
 function Visual(){
   return(
     <section className="content visualM">
       <div className="inner"> 
-      <img src={path +"/img/recordplayer.jpg"} alt="" />
+        <div className="wrap">
+          <div className="banner">
+            <img src={path +"/img/recordplayer.jpg"} alt="" />
+          </div>
+          <div className="stayConnect">
+            <h3>STAY CONNECTED</h3>
+            <ul>
+              <li>
+                <span>Fan S&A on facebook</span>
+                <strong><FontAwesomeIcon icon={faFacebookSquare} /></strong>
+                </li>
+              <li>
+                <span>Follow S&A on Twitter</span>
+                <strong><FontAwesomeIcon icon={faTwitterSquare} /></strong>
+              </li>
+              <li>
+                <span>Like S&A on instagram</span>
+                <strong><FontAwesomeIcon icon={faInstagram} /></strong>
+              </li>
+              <li>
+                <span>Subscribe S&A to RSS fee-ds</span>
+                <strong><FontAwesomeIcon icon={faRssSquare} /></strong>
+              </li>
+              <li>
+                <span>Sign up for newsletter</span>
+                <strong><FontAwesomeIcon icon={faEnvelopeSquare} /></strong>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   )
