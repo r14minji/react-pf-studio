2f4d6ac93e42ab40d43a364c1af5ace0150a2c01
r14minji <rkdalswl0728@gmail.com>
2022-01-15 17:12:57 +0900

    반응형 메뉴 완성

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,34 +1,58 @@
 import { NavLink } from "react-router-dom";
 
-
 function Header(){
   const active = {textDecoration: "line-through"};
   return(
     <header id="header">
       <div className="inner">
-        <h1><NavLink exact to='/'>STUDIOALICE</NavLink></h1>
-        <p>THE CHRONICLES OF A CREATIVE STUDIO</p>
+        {/* 웹 메뉴 */}
+        <nav className="menu_web">
+          <h1><NavLink exact to='/'>STUDIOALICE</NavLink></h1>
+          <p>THE CHRONICLES OF A CREATIVE STUDIO</p>
 
-        <div id="gnb">
-          <ul>
+          <div id="gnb_web">
+            <ul>
+              <li><NavLink activeStyle={active}  to='/about'>ABOUT</NavLink></li>
+              <li><NavLink activeStyle={active}  to='/board'>BOARD</NavLink></li>
+              <li><NavLink activeStyle={active}  to='/gallery'>GALLERY</NavLink></li>
+              <li><NavLink activeStyle={active}  to='/youtube'>YOUTUBE</NavLink></li>
+              <li><NavLink activeStyle={active}  to='/contact'>CONTACT</NavLink></li>
+              <li><NavLink activeStyle={active}  to='/join'>JOIN</NavLink></li>
+            </ul>
+          </div>
+
+          <div className="util_web">
+            <ul>
+              <li><NavLink activeStyle={active}  to='/signin'>Sign In</NavLink></li>
+              <li><NavLink activeStyle={active}  to='/join'>Join</NavLink></li>
+            </ul>
+          </div>
+        </nav>
+
+        {/* 반응형 메뉴 호출 */}
+        <div className="btnCall">
+          <span>메뉴호출</span>
+        </div>
+
+        {/* 반응형 메뉴 */}
+        <nav className="menu_Mo">
+          <h1><NavLink exact to='/'>STUDIOALICE</NavLink></h1>
+          <p>THE CHRONICLES OF A CREATIVE STUDIO</p>
+
+          <ul id="gnb_mo">
             <li><NavLink activeStyle={active}  to='/about'>ABOUT</NavLink></li>
             <li><NavLink activeStyle={active}  to='/board'>BOARD</NavLink></li>
             <li><NavLink activeStyle={active}  to='/gallery'>GALLERY</NavLink></li>
             <li><NavLink activeStyle={active}  to='/youtube'>YOUTUBE</NavLink></li>
             <li><NavLink activeStyle={active}  to='/contact'>CONTACT</NavLink></li>
             <li><NavLink activeStyle={active}  to='/join'>JOIN</NavLink></li>
-          </ul>
-        </div>
-
-        <div className="util">
-          <ul>
             <li><NavLink activeStyle={active}  to='/signin'>Sign In</NavLink></li>
             <li><NavLink activeStyle={active}  to='/join'>Join</NavLink></li>
           </ul>
-        </div>
-      </div>
+        </nav>
 
-    {/* 반응형 메뉴 호출 */}
+
+      </div>
     </header>
   )
 }
