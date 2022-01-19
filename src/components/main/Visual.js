47b8d28732dbb6c1f5833ca031586930d529bfaf
r14minji <rkdalswl0728@gmail.com>
2022-01-19 18:22:26 +0900

    비주얼 슬라이드 구현

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,81 +1,96 @@
-import { faRssSquare, faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
-import {faFacebookSquare, faTwitterSquare, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef, useState } from "react";
+import Anime from "../../class/anime";
 
 const path = process.env.PUBLIC_URL;
 
 function Visual(){
+  const ul = useRef(null);
+  let [index, setIndex] = useState(0);
+  console.log(index)
+
+  const prevSlide = ()=>{
+    if(index === 0) return;
+    setIndex(--index);
+    new Anime(ul.current, {
+      prop : "margin-left", 
+      value : `-${100*index}%`, 
+      duration: 500, 
+    })
+  }
+
+  const nextSlide = ()=>{
+    if(index === 5) return;
+    setIndex(++index);
+    new Anime(ul.current, {
+      prop : "margin-left", 
+      value : `-${100 * index}%`, 
+      duration: 500, 
+    })
+  }
+
+
   return(
     <section className="content visualM">
       <div className="inner"> 
         <div className="wrap">
           <div className="banner">
-            <ul className="slideshow">
+            <ul ref={ul}>
               <li>
-                <img src={path + "/img/visual1.jpg"} alt="" />
-                <div className="txt">
-                  <h1>text</h1>
+                <div className="pic">
+                  <img src={path + "/img/visual1.jpg"} alt="" />
                 </div>
-                </li>
-                <li>
-                <img src={path + "/img/visual1.jpg"} alt="" />
                 <div className="txt">
-                  <h1>text2</h1>
+                  <h1>Attila Marcel _2013</h1>
                 </div>
-                </li>
-                <li>
-                <img src={path + "/img/visual1.jpg"} alt="" />
-                <div className="txt">
-                  <h1>text</h1>
-                </div>
-                </li>
-                <li>
-                <img src={path + "/img/visual1.jpg"} alt="" />
-                <div className="txt">
-                  <h1>text3</h1>
-                </div>
-                </li>
-                <li>
-                <img src={path + "/img/visual1.jpg"} alt="" />
-                <div className="txt">
-                  <h1>text4</h1>
-                </div>
-                </li>
-                <li>
-                <img src={path + "/img/visual1.jpg"} alt="" />
-                <div className="txt">
-                  <h1>text5</h1>
-                </div>
-                </li>
-                <li>
-                <img src={path + "/img/visual1.jpg"} alt="" />
-                <div className="txt">
-                  <h1>text6</h1>
-                </div>
-                </li>
-              </ul>
-          </div>
-
-          <div className="stayConnect">
-            <h3>STAY CONNECTED</h3>
-            <ul>
-              <li>
-                <span>01</span>
-                </li>
-              <li>
-                <span>02</span>
               </li>
               <li>
-                <span>03</span>
+                <div className="pic">
+                  <img src={path + "/img/visual2.jpg"} alt="" />
+                </div>
+                <div className="txt">
+                  <h1>Mommy_2014</h1>
+                </div>
               </li>
               <li>
-                <span>04</span>
+                <div className="pic">
+                  <img src={path + "/img/visual3.jpg"} alt="" />
+                </div>
+                <div className="txt">
+                  <h1>Moonrise Kingdom_2012</h1>
+                </div>
               </li>
               <li>
-                <span>05</span>
+                <div className="pic">
+                  <img src={path + "/img/visual4.jpg"} alt="" />
+                </div>
+                <div className="txt">
+                  <h1>Paterson_ 2016</h1>
+                </div>
+              </li>
+              <li>
+                <div className="pic">
+                  <img src={path + "/img/visual5.jpg"} alt="" />
+                </div>
+                <div className="txt">
+                  <h1>The Florida Project_2017</h1>
+                </div>
+              </li>
+              <li>
+                <div className="pic">
+                  <img src={path + "/img/visual6.jpg"} alt="" />
+                </div>
+                <div className="txt">
+                  <h1>The Grand Budapest Hotel_2014</h1>
+                </div>
               </li>
             </ul>
-            </div>
+
+            <span className="prev" onClick={prevSlide}>prev</span>
+            <span className="next" onClick={nextSlide}>next</span>
+          </div>
+
         </div>
       </div>
     </section>
