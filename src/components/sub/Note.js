fcfa0d9eb3c0ba7416feb94eca846b82a5ab67b5
r14minji <rkdalswl0728@gmail.com>
2022-01-20 19:01:13 +0900

    note 반응형 적용 완료

--- src/components/sub/Note.js
+++ src/components/sub/Note.js
@@ -199,24 +199,8 @@ function Note() {
             })
           }
         </section>
-
-        {/* bookmark */}
-        <section className="bookmark">
-          <p>
-            My Movie
-            <div class="svg_triangle">
-              <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                  <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
-              </svg>
-            </div>
-          </p>
-
-        </section>
-
       </div>
     </main>
-
-
   )
 }
 
