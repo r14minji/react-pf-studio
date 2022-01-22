fb99ba0b3da405c8f09876bd02f4ddfd09959941
r14minji <rkdalswl0728@gmail.com>
2022-01-22 11:50:25 +0900

    store에 reducer를 담아서 App컴포넌트에 전달

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
-
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
