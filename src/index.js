6a6f07ae5094e700e0dfc1b579d1fd972ccb4fb1
r14minji <rkdalswl0728@gmail.com>
2022-01-24 11:45:02 +0900

    redux-persist로 redux데이터 localStorage에 저장해서 새로고침해도 상태값 유지

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import store, {Persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
     <Provider store={store}>
-        <App />
-    </Provider>
+        <PersistGate loading={null} persistor={Persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </HashRouter>
   </React.StrictMode>,
+  
   document.getElementById('root')
 );
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,3 @@
-// export const setMembers= members =>{
-//   return {
-//     type: 'SET_MEMBERS',
-//     payload: members
-//   }
-// }
-
 export const setYoutube = data =>{
   return{
     type: 'SET_YOUTUBE',
