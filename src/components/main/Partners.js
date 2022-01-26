515f93a092b1ddfc4ef1004f56de00cd71cfb1dc
r14minji <rkdalswl0728@gmail.com>
2022-01-26 15:38:42 +0900

    로고 수정 최종

--- src/components/main/Partners.js
+++ src/components/main/Partners.js
@@ -1,10 +1,3 @@
-import { Pagination, Autoplay } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
 const path = process.env.PUBLIC_URL;
 
 function Partners(){
@@ -14,43 +7,18 @@ function Partners(){
         <h1>GLOBAL PARTNERS</h1>
 
         <div className="wrap">
-        <Swiper 
-            modules={[Pagination, Autoplay]}
-            spaceBetween={20}
-            slidesPerView={4}       
-            loop ={true}
-            autoplay={{
-              "delay": 3500,
-              "disableOnInteraction": false
-            }} 
-            pagination={{clickable: true}}
-          >
-            <SwiperSlide>
-              <div className="logo">
-              <img src={path + "/img/partner1.png"} alt="pixar 로고" />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="logo">
-                <img src={path + "/img/partner2.png"} alt="netflx 로고" />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="logo">
-                <img src={path + "/img/partner3.png"} alt="channel 로고" />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="logo">
-                <img src={path + "/img/partner4.png"} alt="apple tv 로고" />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="logo">
-                <img src={path + "/img/partner5.png"} alt="universal 로고" />
-              </div>
-            </SwiperSlide>
-          </Swiper>
+          <div className="logo">
+            <img src={path + "/img/partner1.png"} alt="pixar 로고" />
+          </div>
+          <div className="logo">
+            <img src={path + "/img/partner2.png"} alt="netflx 로고" />
+          </div>
+          <div className="logo">
+            <img src={path + "/img/partner3.png"} alt="channel 로고" />
+          </div>
+          <div className="logo">
+            <img src={path + "/img/partner5.png"} alt="universal 로고" />
+          </div>
         </div>
       </div>
     </section>
