025665d68ff0948d5693fe9525b71b6abdf84d1f
r14minji <rkdalswl0728@gmail.com>
2022-01-26 15:30:56 +0900

    로고 스와이퍼 연결

--- src/components/main/Partners.js
+++ src/components/main/Partners.js
@@ -1,3 +1,10 @@
+import { Pagination, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const path = process.env.PUBLIC_URL;
 
 function Partners(){
@@ -7,21 +14,43 @@ function Partners(){
         <h1>GLOBAL PARTNERS</h1>
 
         <div className="wrap">
-          <div className="logo">
-            <img src={path + "/img/partner1.png"} alt="pixar 로고" />
-          </div>
-          <div className="logo">
-            <img src={path + "/img/partner2.png"} alt="netflx 로고" />
-          </div>
-          <div className="logo">
-            <img src={path + "/img/partner3.png"} alt="channel 로고" />
-          </div>
-          <div className="logo">
-            <img src={path + "/img/partner4.png"} alt="apple tv 로고" />
-          </div>
-          <div className="logo">
-            <img src={path + "/img/partner5.png"} alt="universal 로고" />
-          </div>
+        <Swiper 
+            modules={[Pagination, Autoplay]}
+            spaceBetween={20}
+            slidesPerView={4}       
+            loop ={true}
+            autoplay={{
+              "delay": 3500,
+              "disableOnInteraction": false
+            }} 
+            pagination={{clickable: true}}
+          >
+            <SwiperSlide>
+              <div className="logo">
+              <img src={path + "/img/partner1.png"} alt="pixar 로고" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="logo">
+                <img src={path + "/img/partner2.png"} alt="netflx 로고" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="logo">
+                <img src={path + "/img/partner3.png"} alt="channel 로고" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="logo">
+                <img src={path + "/img/partner4.png"} alt="apple tv 로고" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="logo">
+                <img src={path + "/img/partner5.png"} alt="universal 로고" />
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </section>
