fd267bfe3e109b4af57bc5974264084a7eb8d830
r14minji <rkdalswl0728@gmail.com>
2022-01-26 15:54:25 +0900

    board 코드 정리

--- src/components/sub/Board.js
+++ src/components/sub/Board.js
@@ -1,6 +1,5 @@
-import { useSelector, useDispatch } from "react-redux";
-import { setNews } from '../../redux/actions';
-import { faAngleDoubleRight, faAngleDoubleLeft, faRssSquare, faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import { useSelector } from "react-redux";
+import { faRssSquare, faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import {faFacebookSquare, faTwitterSquare, faInstagram} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,8 +13,6 @@ function Board(){
   const notice = useSelector(state=>state);
   const noticeData = notice.noticeReducer.notice;
   let len = noticeData.length;
-  const dispatch = useDispatch();
-
 
   return(
     <>
