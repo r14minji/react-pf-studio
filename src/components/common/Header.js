ef4ce56af3baf5a6ee67380f4806f5f7fd40faec
r14minji <rkdalswl0728@gmail.com>
2022-01-31 11:15:39 +0900

    header 토글 버튼 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { faBarcode } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Header(props){
   const active = {textDecoration: "line-through"};
   const menuMo = useRef(null);
+  const [isOn, setIsOn] = useState(false);
+  const toggleNav = () => setIsOn(!isOn);
+  const closeNav = () => window.innerWidth > 1200 && setIsOn(false);
+
+  useEffect(()=>{
+    window.addEventListener('resize', closeNav);
+    return()=> window.removeEventListener('resize', closeNav);
+  }, [])
 
   return(
     <header id="header" className={props.type + ' myScroll'}>
@@ -13,7 +21,7 @@ function Header(props){
         {/* 웹 메뉴 */}
         <nav className="menu_web">
           <div className="menu_web_main">
-            <h1><NavLink exact to='/'>STUDIOALICE</NavLink></h1>
+            <h1><NavLink exact to='/' onClick={toggleNav}>STUDIOALICE</NavLink></h1>
             <div className="intro">
               <span>No 41 - UI/UX DEVELPOER 2020</span>
               <p>WEBSITE</p>
@@ -48,21 +56,18 @@ function Header(props){
 
         {/* 반응형 메뉴 호출 */}
         <div className="bg">
-          <div className="btnCall" onClick={e=>{
-            e.currentTarget.classList.toggle("on");
-            menuMo.current.classList.toggle("on");
-          }}>
+          <div id="btnCall" onClick={toggleNav} className={isOn ? 'on' : null}>
             <span>메뉴호출</span>
           </div>
         </div>
 
 
         {/* 반응형 메뉴 */}
-        <nav className="menu_Mo" ref={menuMo}>
-          <h1><NavLink exact to='/'>STUDIOALICE</NavLink></h1>
+        <nav id="menu_Mo" ref={menuMo} className={isOn ? 'on' : null} >
+          <h1><NavLink exact to='/' onClick={toggleNav}>STUDIOALICE</NavLink></h1>
           <p>THE CHRONICLES OF A CREATIVE STUDIO</p>
 
-          <ul id="gnb_mo">
+          <ul id="gnb_mo" onClick={toggleNav}>
             <li><NavLink activeStyle={active}  to='/about'>ABOUT</NavLink></li>
             <li><NavLink activeStyle={active}  to='/board'>BOARD</NavLink></li>
             <li><NavLink activeStyle={active}  to='/gallery'>GALLERY</NavLink></li>
